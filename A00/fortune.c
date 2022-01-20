d5a2deeb58e5749475d3922cf488cbd3dc2a5d77
vagorsol <audrey.butter0930@gmail.com>
2022-01-20 18:44:37 -0500

    Assignment 0 Exercise

---
+++ A00/fortune.c
@@ -0,0 +1,10 @@
+/*
+ * fortune.c
+ * Author: Audrey Yang
+ * Date: 1/20/2022
+ * Prints out a fortune */
+#include <stdio.h>
+
+int main(){
+	printf("Fortune favors the bold, but even bolder is using an idiom instead of a fortune for your fortune program.\n");
+}
