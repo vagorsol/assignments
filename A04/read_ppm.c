cae41a7ee126644cdeb4863cb83491fc2c2fc9c7
vagorsol <audrey.butter0930@gmail.com>
2022-02-14 11:50:22 -0500

    part 1 complete

--- A04/read_ppm.c
+++ A04/read_ppm.c
@@ -1,13 +1,65 @@
+/*
+ * read_ppm.c 
+ * Author: Audrey Yang
+ * Date: 2/14/2022
+ * An implementation of the read_ppm function
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "read_ppm.h"
 
-// TODO: Implement this function
-// Feel free to change the function signature if you prefer to implement an 
-// array of arrays
+// A function that reads PPM files stored in ACII format
+// param: filename (name of PPM file that's being read),
+// w (width of the image in the file), h (height of the image in the file)
+// returns: 2D array of pixels, NULL if there's a reading/malloc error
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  FILE *infile;
+  // check that file can be opened
+  infile = fopen(filename, "r");
+  if(infile == NULL){
+    return NULL;
+  }
+ 
+  // read header
+  int count = 0;
+  char c[32]; // char read in
+  // also. ask if w/h have to be on the same LINE
+  while(count < 3){
+    fgets(c, 32, infile); 
+    if(c[0] != '#'){
+      if(count == 1){
+        // get the dimensions of the picture
+        *w = atoi(&c[0]);
+        *h = atoi(&c[2]);
+      } 
+      count++;
+    }
+    // else: if the line is a comment, basically ignore it
+  } 
 
-  return NULL;
+  struct ppm_pixel *pixels;
+  // check that space can be allocated for array
+  pixels = malloc(sizeof(struct ppm_pixel) * *w * *h);
+  if(pixels == NULL){
+    return NULL; 
+  }
+
+  // read in pixels
+  struct ppm_pixel pixelToAdd;
+  unsigned char r, g, b;
+  for(int i = 0; i < *w; i++){
+    for(int j = 0; j < *h; j++){
+      fscanf(infile, " %hhu %hhu %hhu", &r, &g, &b);
+      pixelToAdd.red = r;
+      pixelToAdd.green = g;
+      pixelToAdd.blue = b;
+      pixels[i * *h + j] = pixelToAdd;
+    }
+  }  
+
+  fclose(infile); 
+  return pixels;
 }
 
--- A04/test_ppm.c
+++ A04/test_ppm.c
@@ -1,10 +1,41 @@
+ /*
+ * test_ppm.c
+ * Author: Audrey Yang
+ * Date: 2/14/2022
+ * A program that tests the read_ppm() function from read_ppm.c
+ * using the file "feep-ascii.ppm"
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
 #include "read_ppm.h"
 
 int main() {
+  struct ppm_pixel *pixels;
+  int width, height;
 
-  // todo: call read_ppm
+  pixels = read_ppm("feep-ascii.ppm", &width, &height);
+  if(pixels == NULL){
+    printf("ERROR: issue in reading the file.\n");
+    exit(1);
+  }
 
+  printf("Testing file feep-ascii.ppm: %d %d\n", width, height);
+  
+  int indx; 
+  for(int i = 0; i < width; i++){
+    for(int j = 0; j < height; j++){
+      indx = i * height + j;
+      printf("(%hhu, %hhu, %hhu) ", 
+              pixels[indx].red, 
+              pixels[indx].green,
+              pixels[indx].blue);
+    }
+    printf("\n");
+  }  
+  
+  free(pixels);
+  pixels = NULL; 
   return 0;
 }
 
