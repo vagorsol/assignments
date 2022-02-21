d02b3336f4447b64a5b601239d111a5f6173a4b1
vagorsol <audrey.butter0930@gmail.com>
2022-02-21 16:58:27 -0500

    read_ppm function complete

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -3,12 +3,43 @@
 #include <string.h>
 #include "read_ppm.h"
 
-// TODO: Implement this function
 // Feel free to change the function signature if you prefer to implement an 
 // array of arrays
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  FILE *infile;
+  // check that file can be opened
+  infile = fopen(filename, "rb");
+  if(infile == NULL){
+    return NULL;
+  }
+ 
+  // read header
+  int count = 0;
+  char c[100]; // char read in, should be large enough if not well. 
+  while(count < 3){
+    fgets(c, 100, infile); 
+    if(c[0] != '#'){
+      if(count == 1){
+        // get the dimensions of the picture
+        *w = atoi(strtok(c," "));
+        *h = atoi(strtok(NULL," ")); 
+      }  
+      count++;
+    }
+  } 
 
-  return NULL;
+  struct ppm_pixel *pixels;
+  pixels = malloc(sizeof(struct ppm_pixel) * *w * *h);
+  if(pixels == NULL){
+    return NULL; 
+  }
+  
+  // this read the file as one large block. each 3 pixels is one chunk
+  // (what sizeof(struct ppm_pixel) does) and it gets put in the array 
+  fread(pixels, sizeof(struct ppm_pixel), *w * *h, infile);
+
+  fclose(infile); 
+  return pixels;
 }
 
 // TODO: Implement this function
