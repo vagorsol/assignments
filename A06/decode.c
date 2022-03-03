23675c0d34da034e663eabff95b14d4114bc6c0b
vagorsol <audrey.butter0930@gmail.com>
2022-03-03 02:26:54 -0500

    finished decoder

--- A06/decode.c
+++ A06/decode.c
@@ -1,7 +1,108 @@
+/*
+ * decode.c
+ * Author: Audrey Yang
+ * Date: 3/2/2022
+ * Decryptes a message hidden in a PPM file.
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
 #include "read_ppm.h"
 
-int main(int argc, char** argv) {
-  return 0;
+int powerOf(int base, int power){
+  if(power == 0){
+    return 1;
+  }
+  int ret = 1;  
+  for(int i = 0; i < power; i++){
+    ret = ret * base;
+  }
+  return ret;
 }
 
+int main(int argc, char** argv) {
+  struct ppm_pixel *pixels;
+  int width, height, maxchar;
+  
+  if(argv[1] == NULL){
+    printf("No file to open!\n");
+    exit(1);
+  }
+  
+  pixels = read_ppm(argv[1], &width, &height);
+  if(pixels == NULL){
+    printf("ERROR: Cannot open the file: %s\n", argv[1]);
+    exit(1);
+  }
+  maxchar = (width * height * 3) / 8; 
+  
+  printf("Reading %s with width %d and height %d\n", argv[1], width, height);
+  printf("Max number of characters in the image: %d\n", maxchar);
+
+  int *secretchar;
+  secretchar = malloc(sizeof(int) * maxchar * 8);
+  if(secretchar == NULL){
+    printf("ERROR: unable to write secret message!\n");
+    exit(1);
+  }
+  unsigned int mask = 0x1;
+  int indx; // 2d array indexing
+  int count = 0; // letter array indexing
+  // take each least significant bit from each color value
+  // then put it in and read 
+  for(int i = 0; i < height; i++){
+    for(int j = 0; j < width; j++){
+      indx = i * width + j; 
+      /*printf("%1X\t%d\n", pixels[indx].red, pixels[indx].red);
+      printf("%1X\t%d\n", pixels[indx].green, pixels[indx].green);
+      printf("%1X\t%d\n", pixels[indx].blue, pixels[indx].blue);*/
+
+      // get all the least significant bits
+      // the masks are not right. why.
+      secretchar[count] = mask & pixels[indx].red;
+      secretchar[count + 1] = mask & pixels[indx].green;
+      secretchar[count + 2] = mask & pixels[indx].blue;
+      count = count + 3;
+    }
+  }
+
+  char *secretmsg = NULL;
+  secretmsg = calloc(maxchar, sizeof(char));
+  if(secretmsg == NULL){
+    printf("ERROR: unable to write secret message!\n");
+    exit(1);
+  }
+
+  int secretletter = 0;
+  count = 0;
+  indx = 0;
+  // read bits, detect null char
+  for(int i = 0; i < maxchar * 8; i++){
+    if(count == 8){
+      secretmsg[indx] = secretletter;
+      // printf("%d\t%c\n", secretletter, secretmsg[indx]);
+      // add to arr
+      if(secretletter == 0){
+        // if null terminator - stop
+        break; 
+      } else{
+        secretletter = 0;
+        indx = indx + 1;
+        count = 0;
+      }
+    } 
+    // printf("Count : %d\t Char: %d\n",count, secretchar[i]);
+    secretletter = secretletter + (secretchar[i] * powerOf(2,7 - count)); 
+    // printf("Status: %d\n", secretletter);
+    count = count + 1;
+  }
+  printf("%s\n", secretmsg);
+
+  free(secretchar);
+  secretchar = NULL;
+  free(secretmsg);
+  secretmsg = NULL;
+  free(pixels);
+  pixels = NULL; 
+  return 0;
+}
--- A06/read_ppm.c
+++ A06/read_ppm.c
@@ -1,19 +1,83 @@
+/*
+ * read_ppm.c
+ * Author: Audrey Yang
+ * Date: 2/23/2022
+ * An implementation of the read_ppm() and write_ppm() functions from read_ppm.h
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "read_ppm.h"
 
-// TODO: Implement this function
-// Feel free to change the function signature if you prefer to implement an 
-// array of arrays
+/*
+ * Reads a PPM file in binary format and returns an array with all the RGB
+ * values of all the pixels
+ */
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  FILE *infile;
+  // check that file can be opened
+  infile = fopen(filename, "rb");
+  if(infile == NULL){
+    return NULL;
+  } 
+  // read header
+  int count = 0;
+  // char filetype[3];
+  char c[100]; 
+  while(count < 3){
+    fgets(c, 100, infile); 
+    if(c[0] != '#'){
+      // getting the dimensions of the picture
+      if(count == 1){
+        *w = atoi(strtok(c," "));
+        *h = atoi(strtok(NULL," ")); 
+      }  
+      count++;
+    }
+  } 
 
-  return NULL;
+  struct ppm_pixel *pixels;
+  pixels = malloc(sizeof(struct ppm_pixel) * *w * *h);
+  if(pixels == NULL){
+    return NULL; 
+  }
+   
+  fread(pixels, sizeof(struct ppm_pixel), *w * *h, infile);
+
+  fclose(infile); 
+  return pixels;
 }
 
-// TODO: Implement this function
-// Feel free to change the function signature if you prefer to implement an 
-// array of arrays
+/*
+ * Given an array of pixels, write a new PPM file with the given pixels
+ */
 extern void write_ppm(const char* filename, struct ppm_pixel* pxs, int w, int h) {
+  FILE *infile;
+  infile = fopen(filename, "wb");
+  if(infile == NULL){
+    printf("Error: unable to open outfile.\n");
+    exit(1);
+  }
 
+  // header information
+  char filetype[] = "P6\n";
+  char *arrsize;
+  arrsize = malloc(sizeof(int) * 5);
+  if(arrsize == NULL){
+    printf("ERROR: unable to write header.\n");
+    exit(1);
+  }
+  sprintf(arrsize,"%d %d\n", w, h);
+  char maxpixval[] = "225\n";
+  fwrite(filetype, sizeof(filetype) - 1, 1, infile);
+  fwrite(arrsize, sizeof(arrsize), 1, infile);
+  fwrite(maxpixval, sizeof(maxpixval) - 1, 1, infile);
+  
+  // TODO: write here    
+ 
+  fwrite(pxs, sizeof(struct ppm_pixel), w * h, infile);
+  fclose(infile);
+  free(arrsize);
+  arrsize = NULL;
 }
