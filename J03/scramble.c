c5a6caddf2c52e70d0ece5256ee3168a1ac49429
vagorsol <audrey.butter0930@gmail.com>
2022-03-17 16:10:17 -0400

    fixed incorrect non-swapped-bit masking

--- J03/scramble.c
+++ J03/scramble.c
@@ -3,16 +3,13 @@
 #include <stdlib.h>
 
 unsigned char scramble(char input){
-  unsigned int bit1 = input & 0x8;
+  unsigned int bitoop = input & 0xF9;
   unsigned int bit2 = input & 0x4;
   unsigned int bit3 = input & 0x2;
-  unsigned int bit4 = input & 0x1;
   unsigned char ret; 
-  // TODO: doesn't return right character
   bit2 = bit2 >> 1;
   bit3 = bit3 << 1;
-  ret = (unsigned char) (bit1 | bit2 | bit3 | bit4);
-  // printf("Input: %X Output: %X\n", input, ret);
+  ret = (unsigned char) (bitoop | bit2 | bit3);
   return ret; 
 }
 
@@ -21,12 +18,11 @@ int main() {
   printf("Please enter a phrase: ");
   scanf(" %s", input);
   
-  char* output;
-  output = calloc(sizeof(char) * strlen(input), strlen(input) + 1);  
+  unsigned char* output;
+  output = calloc(sizeof(unsigned char) * strlen(input), strlen(input) + 1);  
   // loop through all the characters in the given string
   for(int i = 0; i < strlen(input); i++){
     output[i] = scramble(input[i]);  
-    printf("%d", output[i]);
   }
   printf("scramble: %s\n", output);
   free(output);
